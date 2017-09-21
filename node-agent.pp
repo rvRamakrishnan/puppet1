b90222f22e806c2af106eb36176b211c1e3fc103
Ramakrishnan Venkataramanan <ramakrishnan.v@live.com>
2017-09-21 06:01:35 +0000

    syntax errors in node-agent.pp manifest file fixed

--- node-agent.pp
+++ node-agent.pp
@@ -1,5 +1,6 @@
-node 'puppet-agent1' { '/tmp/agentonly.txt':
-{
-    ensure => file,
-    content => "Created on Agent Nodes only",
-}
+node 'puppet-agent1' {
+    file { '/tmp/agentonly.txt':
+            ensure => file,
+            content => "Created on Agent Nodes only",
+        }
+}     
